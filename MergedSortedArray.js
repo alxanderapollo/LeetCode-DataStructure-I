f9349bc150777f642e2d28e89a438f7dcc09ceba
Alex <alexsalazarny@gmail.com>
2022-05-27 12:49:59 -0400

    sol number of steps leet code daily

---
+++ MergedSortedArray.js
@@ -0,0 +1,22 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  //for the first array the 0s represent all of the nums that we dont want and are always at the end of the array
+  //1 iterate through the elements
+  for (let i = 0; i < n; i++) {
+    //2Append the values of nums2 at the end of nums1
+    nums1[i + m] = nums2[i];
+  }
+  nums1.sort();
+};
